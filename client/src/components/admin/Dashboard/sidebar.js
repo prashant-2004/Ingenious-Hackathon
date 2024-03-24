170d0d05747f9e4023c8a7bf195593734960a0d1
Pranav <pranavshelke4@gmail.com>
2024-03-24 10:35:04 +0530

    Refactor sidebar component to use useState hook and update navigation links

--- client/src/components/admin/Dashboard/sidebar.js
+++ client/src/components/admin/Dashboard/sidebar.js
@@ -1,10 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../../style/admin/dashboard/sidebar.css";
 import AppointmentIcon from "../../../assets/admin/Appointment.svg";
 import ServiceIcon from "../../../assets/admin/Service.svg";
 import GoalsIcon from "../../../assets/admin/Goal.svg";
 
-function sidebar() {
+function Sidebar() {
+  const [activeLink, setActiveLink] = useState(0);
+
+  const navLinks = [
+    {
+      name: "Dashboard",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <g clip-path="url(#clip0_16_4420)">
+            <path
+              d="M4 4H10V12H4V4Z"
+              stroke="#FEFEFE"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M4 16H10V20H4V16Z"
+              stroke="#FEFEFE"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M14 12H20V20H14V12Z"
+              stroke="#FEFEFE"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M14 4H20V8H14V4Z"
+              stroke="#FEFEFE"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </g>
+          <defs>
+            <clipPath id="clip0_16_4420">
+              <rect width="24" height="24" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
+      ),
+    },
+    {
+      name: "Appointment",
+      icon: <img alt="Appointment" src={AppointmentIcon} />,
+    },
+    { name: "Service", icon: <img alt="Service" src={ServiceIcon} /> },
+    { name: "Goals", icon: <img alt="Goals" src={GoalsIcon} /> },
+  ];
+
   return (
     <div className="sidebar">
       <div className="logo">
@@ -12,70 +71,19 @@ function sidebar() {
       </div>
       <h6>MENU</h6>
       <div className="nav-links">
-        <div className="nav-link">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
+        {navLinks.map((link, index) => (
+          <div
+            className={`nav-link ${index === activeLink ? "active" : ""}`}
+            onClick={() => setActiveLink(index)}
+            key={index}
           >
-            <g clip-path="url(#clip0_16_4420)">
-              <path
-                d="M4 4H10V12H4V4Z"
-                stroke="#FEFEFE"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M4 16H10V20H4V16Z"
-                stroke="#FEFEFE"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M14 12H20V20H14V12Z"
-                stroke="#FEFEFE"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M14 4H20V8H14V4Z"
-                stroke="#FEFEFE"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </g>
-            <defs>
-              <clipPath id="clip0_16_4420">
-                <rect width="24" height="24" fill="white" />
-              </clipPath>
-            </defs>
-          </svg>
-          Dahboard
-        </div>
-
-        <div className="nav-link">
-          <img alt="Appointment" src={AppointmentIcon} />
-          Appointment
-        </div>
-
-        <div className="nav-link">
-          <img alt="Service" src={ServiceIcon} />
-          Service
-        </div>
-
-        <div className="nav-link">
-          <img alt="Goals" src={GoalsIcon} />
-          Goals
-        </div>
+            {link.icon}
+            {link.name}
+          </div>
+        ))}
       </div>
     </div>
   );
 }
 
-export default sidebar;
+export default Sidebar;
