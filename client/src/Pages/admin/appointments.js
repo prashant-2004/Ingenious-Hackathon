fa622baab9501e5fad647c3c0c090d0827333782
Pranav <pranavshelke4@gmail.com>
2024-03-25 19:11:32 +0530

    Refactor admin appointments page and add new component for displaying all appointments

--- client/src/Pages/admin/appointments.js
+++ client/src/Pages/admin/appointments.js
@@ -1,10 +1,27 @@
 import React from "react";
-import Sidebar from "../../components/admin/Dashboard/sidebar";
+import AdminSidebar from "../../components/admin/Dashboard/sidebar";
+import AllAppoinments from "../../components/admin/AllAppoinments";
 
 function appointments() {
   return (
-    <div>
-      <Sidebar />
+    <div
+      style={{
+        display: "flex",
+        background: "var(--Surface, #F2F5FA)",
+        height: "100vh",
+      }}
+    >
+      <AdminSidebar />
+      <div
+        style={{
+          marginLeft: "12.5%",
+          width: "100%",
+          height: "100%",
+          padding: "0% 3%",
+        }}
+      >
+        <AllAppoinments />
+      </div>
     </div>
   );
 }
--- client/src/components/admin/AllAppoinments.js
+++ client/src/components/admin/AllAppoinments.js
@@ -1,7 +1,6 @@
 import React from "react";
-import "../../../style/admin/dashboard/ScheduleAppointment.css";
 
-function ScheduleAppointment() {
+function AllAppoinments() {
   return (
     <div className="Schedule-Appointment">
       <h6>Appointment Schedule</h6>
@@ -10,28 +9,46 @@ function ScheduleAppointment() {
           <table className="Schedule-Appointment-table">
             <thead className="Schedule-Appointment-table-thead">
               <tr>
+                <th>Id</th>
                 <th>Name</th>
                 <th>Service</th>
                 <th>Date</th>
                 <th>Time</th>
+                <th>Address</th>
+                <th>Email</th>
                 <th>Phone</th>
+                <th>Action</th>
               </tr>
             </thead>
             <tbody className="Schedule-Appointment-table-tbody">
               <tr>
+                <td>1</td>
                 <td>John Doe</td>
                 <td>Haircut</td>
                 <td>1/1/2024</td>
                 <td>10:00 AM</td>
-                <td>+91 1234567890</td>
+                <td>123, ABC Street</td>
+                <td>abc@gmail.com</td>
+                <td>1234567890</td>
+                <td className="action-btns">
+                  <button className="Accept-btn">Accept</button>
+                  <button className="Reject-btn">Reject</button>
+                </td>
               </tr>
 
               <tr>
+                <td>2</td>
                 <td>John Doe</td>
                 <td>Haircut</td>
                 <td>1/1/2024</td>
                 <td>10:00 AM</td>
-                <td>+91 1234567890</td>
+                <td>123, ABC Street</td>
+                <td>abc@gmail.com</td>
+                <td>1234567890</td>
+                <td className="action-btns">
+                  <button className="Accept-btn">Accept</button>
+                  <button className="Reject-btn">Reject</button>
+                </td>
               </tr>
             </tbody>
           </table>
@@ -41,4 +58,4 @@ function ScheduleAppointment() {
   );
 }
 
-export default ScheduleAppointment;
+export default AllAppoinments;
