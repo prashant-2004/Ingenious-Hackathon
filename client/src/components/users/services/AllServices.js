bfdad54f53c3c27b6d5fa87123159a51c7264ddc
Pranav <pranavshelke4@gmail.com>
2024-04-06 01:47:37 +0530

    Fix tab rendering issue and update AllServices component in client/src/components/users/services/AllServices.js

--- client/src/components/users/services/AllServices.js
+++ client/src/components/users/services/AllServices.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "../../../style/user/services/AllServices.css";
 import ServiceCards from "./ServiceCards";
-import  Appoinmentform from'../services/Appoinmentform'
+import Appoinmentform from "../services/Appoinmentform";
 
 function AllServices() {
   const [activeTab, setActiveTab] = useState("tab1");
@@ -12,34 +12,38 @@ function AllServices() {
 
   return (
     <>
-    <div className="AllServices">
-      <div className="tab-container">
-        <div
-          className={`tab ${activeTab === "tab1" ? "active" : ""}`}
-          onClick={() => handleTabClick("tab1")}
-        >
-          Tab 1
+      <div className="AllServices">
+        <div className="tab-container">
+          <div
+            className={`tab ${activeTab === "tab1" ? "active" : ""}`}
+            onClick={() => handleTabClick("tab1")}
+          >
+            Tab 1
+          </div>
+          <div
+            className={`tab ${activeTab === "tab2" ? "active" : ""}`}
+            onClick={() => handleTabClick("tab2")}
+          >
+            Tab 2
+          </div>
+          <div
+            className={`tab ${activeTab === "tab3" ? "active" : ""}`}
+            onClick={() => handleTabClick("tab3")}
+          >
+            Tab 3
+          </div>
         </div>
-        <div
-          className={`tab ${activeTab === "tab2" ? "active" : ""}`}
-          onClick={() => handleTabClick("tab2")}
-        >
-          Tab 2
-        </div>
-        <div
-          className={`tab ${activeTab === "tab3" ? "active" : ""}`}
-          onClick={() => handleTabClick("tab3")}
-        >
-          Tab 3
+        <div className="tab-content">
+          {activeTab === "tab1" && (
+            <div>
+              <ServiceCards />
+            </div>
+          )}
+          {activeTab === "tab2" && <div>Content for Tab 2</div>}
+          {activeTab === "tab3" && <div>Content for Tab 3</div>}
         </div>
       </div>
-      <div className="tab-content">
-        {activeTab === "tab1" && <div><ServiceCards /></div>}
-        {activeTab === "tab2" && <div>Content for Tab 2</div>}
-        {activeTab === "tab3" && <div>Content for Tab 3</div>}
-      </div>
-    </div>
-    <Appoinmentform />
+      <Appoinmentform />
     </>
   );
 }
